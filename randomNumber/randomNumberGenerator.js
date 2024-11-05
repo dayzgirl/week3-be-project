c1a7f43fb2419d92a063e8d9d994e3187aa47766
dayzgirl <69809989+dayzgirl@users.noreply.github.com>
2024-11-05 17:31:36 -0500

    Initial commit

---
+++ randomNumber/randomNumberGenerator.js
@@ -0,0 +1,9 @@
+
+const randomNumber = Math.floor(Math.random() * 100) + 1;
+
+if (randomNumber % 2 === 0) {
+    console.log(`The number ${randomNumber} is even.`);
+} else {
+    console.log(`The number ${randomNumber} is odd.`);
+}
+// Random number generated
